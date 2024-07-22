969a824ada857f2a43be4a8cada2bd9d2670e695
Fantasy AI Studio <alan.huang67@gmail.com>
2024-07-22 18:37:38 +0800

    Update fai_voronoi_generator.py

--- nodes/fai_voronoi_generator.py
+++ nodes/fai_voronoi_generator.py
@@ -23,16 +23,16 @@ class FAI_Voronoi_Generator:
             },
             "optional": {
                 "distance_metric": ([
-                    "euclidean (圆形)",
-                    "manhattan (矩形)",
-                    "chebyshev (菱形)",
-                    "minkowski (圆方形)",
-                    "elliptical (椭圆形)",
-                    "kaleidoscope_star (万花筒_旋转星)",
-                    "kaleidoscope_wave (万花筒_波浪)",
-                    "kaleidoscope_radiation_α (万花筒_放射线_α)",
-                    "kaleidoscope_radiation_β (万花筒_放射线_β)",
-                    "kaleidoscope_radiation_γ (万花筒_放射线_γ)"
+                    "euclidean",
+                    "manhattan",
+                    "chebyshev",
+                    "minkowski",
+                    "elliptical",
+                    "kaleidoscope_star",
+                    "kaleidoscope_wave",
+                    "kaleidoscope_radiation_α",
+                    "kaleidoscope_radiation_β",
+                    "kaleidoscope_radiation_γ"
                 ],),
                 "x_schedule": ("LIST",),
                 "y_schedule": ("LIST",),
@@ -60,23 +60,23 @@ class FAI_Voronoi_Generator:
     
 class FAIScaleScheduler:
     formulas = {
-        "高频振荡（High-Frequency Oscillation） - 适合高频节奏的音乐": "0:(15 * cos((a / 60 * 3.141 * (t + 0) / b))**70 + 0.009)",
-        "非线性变化（Nonlinear Variation） - 适合不规则节奏的音乐": "0:(10 * abs(tan(t / b * 3.141 * (a / 60))) % 1 + 0.01)",
-        "重复线性变化（Repeating Linear Change） - 适合节奏鲜明的音乐": "0:(10 * abs(2 * ((t / b * a / 60) % 1) - 1) + 0.01)",
-        "平滑周期变化（Smooth Periodic Change）[Sin] - 适合柔和的音乐节奏": "0:(10 * abs(sin(t / b * 2 * 3.141 * (a / 60))) + 0.01)",
-        "平滑周期变化（Smooth Periodic Change）[Cosine] - 适合柔和的音乐节奏": "0:(10 * abs(cos(t / b * 2 * 3.141 * (a / 60))) + 0.01)",
-        "二次曲线变化（Quadratic Curve Change） - 适合渐变的音乐节奏": "0:((t / b * a / 60)**2 % 1 * 10 + 0.01)",
-        "三次曲线变化（Cubic Curve Change） - 适合渐进的音乐节奏": "0:((t / b * a / 60)**3 % 1 * 10 + 0.01)",
-        "对数变化（Logarithmic Change） - 适合渐强的音乐节奏": "0:(10 * log(t / b * a / 60 + 1) % 1 + 0.01)",
-        "线性绝对值变化（Linear Absolute Change） - 适合明确节奏的音乐": "0:(10 * abs(t / b * a / 60 % 1) + 0.01)",
-        "指数变化（Exponential Change） - 适合逐渐增强的音乐效果": "0:((exp(t / b * a / 60) - 1) / (exp(1) - 1) * 10 + 0.01)",
-        "柔和波浪变化（Gentle Wave Change） - 适合轻柔的音乐节奏": "0:(5 * sin(2 * 3.141 * (t / b) * (a / 60))**2 + 0.01)",
-        "温和振荡变化（Gentle Oscillation Change） - 适合轻松的音乐节奏": "0:(5 * (cos(3.141 * (t / b) * (a / 60))**3 + 1) / 2 + 0.01)",
-        "平滑递增变化（Smooth Incremental Change） - 适合舒缓的音乐节奏": "0:(5 * (1 - exp(-3 * (t / b) * (a / 60))) + 0.01)",
-        "S型曲线变化（S-Curve Change） - 适合渐进和渐出效果": "0:(10 / (1 + exp(-10 * ((t / b * a / 60) - 0.5))) + 0.01)",
-        "脉冲变化（Pulse Change） - 适合脉冲和短促节奏": "0:(10 * (t % (b / a) < (b / a) / 2) + 0.01)",
-        "抛物线变化（Parabolic Change） - 适合渐变和峰值节奏": "0:(10 * (1 - ((t / b * a / 60 - 0.5) ** 2) * 4) + 0.01)",
-        "锯齿波变化（Sawtooth Change） - 适合急剧上升和下降的节奏": "0:(10 * ((t / b * a / 60) % 1) + 0.01)"
+        "High-Frequency Oscillation": "0:(15 * cos((a / 60 * 3.141 * (t + 0) / b))**70 + 0.009)",
+        "Nonlinear Variation": "0:(10 * abs(tan(t / b * 3.141 * (a / 60))) % 1 + 0.01)",
+        "Repeating Linear Change": "0:(10 * abs(2 * ((t / b * a / 60) % 1) - 1) + 0.01)",
+        "Smooth Periodic Change [Sin]": "0:(10 * abs(sin(t / b * 2 * 3.141 * (a / 60))) + 0.01)",
+        "Smooth Periodic Change [Cosine]": "0:(10 * abs(cos(t / b * 2 * 3.141 * (a / 60))) + 0.01)",
+        "Quadratic Curve Change": "0:((t / b * a / 60)**2 % 1 * 10 + 0.01)",
+        "Cubic Curve Change": "0:((t / b * a / 60)**3 % 1 * 10 + 0.01)",
+        "Logarithmic Change": "0:(10 * log(t / b * a / 60 + 1) % 1 + 0.01)",
+        "Linear Absolute Change": "0:(10 * abs(t / b * a / 60 % 1) + 0.01)",
+        "Exponential Change": "0:((exp(t / b * a / 60) - 1) / (exp(1) - 1) * 10 + 0.01)",
+        "Gentle Wave Change": "0:(5 * sin(2 * 3.141 * (t / b) * (a / 60))**2 + 0.01)",
+        "Gentle Oscillation Change": "0:(5 * (cos(3.141 * (t / b) * (a / 60))**3 + 1) / 2 + 0.01)",
+        "Smooth Incremental Change": "0:(5 * (1 - exp(-3 * (t / b) * (a / 60))) + 0.01)",
+        "S-Curve Change": "0:(10 / (1 + exp(-10 * ((t / b * a / 60) - 0.5))) + 0.01)",
+        "Pulse Change": "0:(10 * (t % (b / a) < (b / a) / 2) + 0.01)",
+        "Parabolic Change": "0:(10 * (1 - ((t / b * a / 60 - 0.5) ** 2) * 4) + 0.01)",
+        "Sawtooth Change": "0:(10 * ((t / b * a / 60) % 1) + 0.01)"
     }
     @classmethod
     def INPUT_TYPES(cls):
