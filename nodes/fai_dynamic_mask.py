b0c16d41f505edd1bd088789c281333c0cdef1bf
Fantasy AI Studio <alan.huang67@gmail.com>
2024-07-30 11:22:41 +0800

    Update fai_dynamic_mask.py

--- nodes/fai_dynamic_mask.py
+++ nodes/fai_dynamic_mask.py
@@ -10,7 +10,7 @@ class FAIDynamicMask:
                 "width": ("INT", {"default": 512, "min": 1}),
                 "height": ("INT", {"default": 512, "min": 1}),
                 "frame_count": ("INT", {"default": 30, "min": 1}),
-                "shape_transition": ([
+                "shape_transition": ([ 
                     "fade_in", "fade_out",
                     "circle_out", "circle_in",
                     "square_out", "square_in",
@@ -19,7 +19,7 @@ class FAIDynamicMask:
                     "top_to_bottom", "bottom_to_top"
                 ],),
                 "feather": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 1.0}),
-                "ease": (["linear", "ease_in", "ease_out", "ease_in_out"],)
+                "weight_type": (["linear", "ease_in", "ease_out", "ease_in_out"],)
             },
         }
 
@@ -28,36 +28,36 @@ class FAIDynamicMask:
     FUNCTION = "generate_mask"
     CATEGORY = "⚛️FAI_Node⚛️"
 
-    def generate_mask(self, width, height, frame_count, shape_transition, feather, ease):
+    def generate_mask(self, width, height, frame_count, shape_transition, feather, weight_type):
         mask_sequence = []
         invert_mask_sequence = []
 
         for frame in range(frame_count):
-            mask = self.create_mask(frame, frame_count, shape_transition, width, height, feather, ease)
+            mask = self.create_mask(frame, frame_count, shape_transition, width, height, feather, weight_type)
             invert_mask = 1 - mask
             mask_sequence.append(mask)
             invert_mask_sequence.append(invert_mask)
 
         return (torch.stack(mask_sequence), torch.stack(invert_mask_sequence))
 
-    def create_mask(self, frame, frame_count, shape_transition, width, height, feather, ease):
+    def create_mask(self, frame, frame_count, shape_transition, width, height, feather, weight_type):
         mask = np.zeros((height, width), dtype=np.float32)
         t = frame / (frame_count - 1)
 
-        if ease == "linear":
+        if weight_type == "linear":
             progress = t
-        elif ease == "ease_in":
+        elif weight_type == "ease_in":
             progress = t ** 2.0
-        elif ease == "ease_out":
+        elif weight_type == "ease_out":
             progress = t ** (1 / 2.0)
-        elif ease == "ease_in_out":
+        elif weight_type == "ease_in_out":
             progress = (np.sin((t - 0.5) * np.pi) / 2) + 0.5
 
         if shape_transition == "fade_in":
             mask[:, :] = progress
         elif shape_transition == "fade_out":
             mask[:, :] = 1 - progress
-            
+
         elif shape_transition == "circle_out":
             max_dim = np.sqrt(width ** 2 + height ** 2)
             radius = progress * (max_dim / 2) + 2  # 增加2像素以确保覆盖
@@ -89,7 +89,7 @@ class FAIDynamicMask:
         elif shape_transition == "diamond_out":
             diag_length = np.sqrt(width ** 2 + height ** 2)
             side = progress * diag_length * np.sqrt(2)  # 将边长乘以√2以确保对角线等于画布对角线
-            points = np.array([[
+            points = np.array([[ 
                 (width // 2, height // 2 - side // 2),  # Top
                 (width // 2 + side // 2, height // 2),  # Right
                 (width // 2, height // 2 + side // 2),  # Bottom
@@ -100,7 +100,7 @@ class FAIDynamicMask:
         elif shape_transition == "diamond_in":
             diag_length = np.sqrt(width ** 2 + height ** 2)
             side = (1 - progress) * diag_length * np.sqrt(2)  # 将边长乘以√2以确保对角线等于画布对角线
-            points = np.array([[
+            points = np.array([[ 
                 (width // 2, height // 2 - side // 2),  # Top
                 (width // 2 + side // 2, height // 2),  # Right
                 (width // 2, height // 2 + side // 2),  # Bottom
@@ -108,7 +108,6 @@ class FAIDynamicMask:
             ]], dtype=np.int32)
             mask = cv2.fillPoly(mask, points, 1)
             mask = 1 - mask
-
 
         elif shape_transition == "left_to_right":
             max_dim = width
@@ -126,15 +125,12 @@ class FAIDynamicMask:
             max_dim = height
             mask = cv2.rectangle(mask, (0, int((1 - progress) * max_dim)), (width, height), 1, -1)
 
-
-
         if feather > 0:
             mask = cv2.GaussianBlur(mask, (0, 0), feather * min(width, height))
 
         mask[mask < 1e-3] = 0
 
         return torch.tensor(mask).unsqueeze(0)
-
 
 # 节点映射
 NODE_CLASS_MAPPINGS = {
